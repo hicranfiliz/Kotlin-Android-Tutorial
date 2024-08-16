15ac99748ffff8c824535dab3527b399ab7bfb51
hicranfiliz <h.filiz781@gmail.com>
2024-08-17 01:31:51 +0300

    added shared preferenecs

--- app/src/main/java/com/example/sampleapp/SPDemoActivity.kt
+++ app/src/main/java/com/example/sampleapp/SPDemoActivity.kt
@@ -1,20 +1,49 @@
 package com.example.sampleapp
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SPDemoActivity : AppCompatActivity() {
+    private lateinit var nameText : EditText
+    private lateinit var ageText : EditText
+    private lateinit var sp : SharedPreferences
+    private lateinit var editor : SharedPreferences.Editor
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_spdemo)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        nameText  = findViewById(R.id.etName)
+        ageText = findViewById(R.id.etage)
+        sp = getSharedPreferences("my_sp", MODE_PRIVATE)
+        editor = sp.edit()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val name  = nameText.text.toString()
+        val age = ageText.text.toString().toInt()
+        editor.apply{
+            putString("sp_name", name)
+            putInt("sp_age", age)
+            commit()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val name = sp.getString("sp_name", null)
+        val age = sp.getInt("sp_age", 0)
+
+        nameText.setText(name)
+        if (age != 0){
+            ageText.setText(age.toString())
+        }
+
     }
 }
